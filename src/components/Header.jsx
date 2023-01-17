71a43fbb50da7062cfd66a91b9acae6c380df5c9
yoniescobar <yoniedilzar@gmail.com>
2023-01-16 21:15:16 -0600

    se agrego texto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
       Seguimiento Pacientes {""}
-      <span className='text-indigo-600'>Veterinaria. S.A </span>
+      <span className='text-indigo-600'>Veterinaria. S.A. </span>
     </h1>
   )
 }
